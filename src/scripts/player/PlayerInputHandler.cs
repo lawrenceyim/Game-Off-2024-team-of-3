00d28d5ae59bb3f5ea433a72cc0c01c7e261ee42
Lawrence Y <84044148+lawrenceyim@users.noreply.github.com>
2024-11-02 19:45:11 -0400

    Refactored directory structure and folder names.

---
+++ src/scripts/player/PlayerInputHandler.cs
@@ -0,0 +1,34 @@
+using Godot;
+using System;
+
+public partial class PlayerInputHandler : Node, IInputListener {
+	[Export] private PlayerMovement _playerMovement;
+
+	public override void _Ready() {
+		InputManager.GetInstance().AddListener(this);
+	}
+
+	public void ProcessInput() {
+		Vector2 inputVector = Vector2.Zero;
+		if (Input.IsActionPressed("MoveUp")) {
+			inputVector.Y -= 1;
+		}
+		if (Input.IsActionPressed("MoveDown")) {
+			inputVector.Y += 1;
+		}
+		if (Input.IsActionPressed("MoveLeft")) {
+			inputVector.X -= 1;
+		}
+		if (Input.IsActionPressed("MoveRight")) {
+			inputVector.X += 1;
+		}
+		_playerMovement.Move(inputVector.Normalized());
+		
+		if (Input.IsActionJustPressed("Action")) {
+			// Player Action
+		}
+		if (Input.IsActionJustPressed("Escape")) {
+			// Escape menu
+		}
+	}
+}
