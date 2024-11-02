ab687e6a71cc199383856cd839854f3941cccc82
Lawrence Y <84044148+lawrenceyim@users.noreply.github.com>
2024-11-02 15:34:20 -0400

    Updated DialogueManager to use a .tsv instead of Excel for text.

--- Scripts/Dialogue/DialogueManager.cs
+++ Scripts/Dialogue/DialogueManager.cs
@@ -1,32 +1,73 @@
 /*
 	Add as AUTOLOAD
 	
-	Intended to be used with an Excel spreadsheet that contains dialogue that will be parsed and stored in memory.
-	Excel columns should have this format:
-	A: dialogue_key; a unique key that identifies the dialogue text/node. i.e. hailey_intro_speech_1
-	B: node_type; text or question
-	C: next_node_key; 1 or more dialogue keys that identifies the next dialogue. "end" is used to indicate end of dialgoue.
-		i.e. hailey_intro_speech_2, hailey_angry_speech_1, end
-	D: icon_key; identifies the icon that should be used for the dialogue. i.e. hailey_happy_face
-		Use "none" to indicate no icon
-	E: ##_text; the actual text to be displayed. This can be the text for a dialogue, question, or question answer.
-		i.e. Hailey is very happy to be with you.
-		Each row from E...etc is used for different langauge localization. E is English, G is Spanish, etc.
-		## indicates the language.
-		i.e. english_text
+	dialogueDict will contain all the in-game text using a key-value pair.
+	The key will be the text's unique ID.
+	The value will be the text in the appropriate language, selected in the settings.
+
+	Text will be read from a tab separated value (.tsv) file that contains all the texts with their unique ID and translations.
+	Example:
+	dialogue_key	english_text	spanish_text
+	greeting_1	hello	hola
+	bye_1	bye	adios
+
+	Use an Google spreadsheet to organize these text and download it as a .tsv file.
+	
+	DO NOT EDIT TSV FILE IN VS CODE. TABS ARE NOT INSERTED CORRECTLY. EDIT IN GOOGLE SHEETS AND DOWNLOAD AS TSV.
 */
 
 using Godot;
+using System;
+using System.Collections.Generic;
+using System.IO;
 
 public partial class DialogueManager : Node
 {
+	private const string textFilePath = "Resources/text.tsv";
+	private string language = "english_text";
+	private Dictionary<string, string> textDict = new Dictionary<string, string>();
+	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		GD.Print("DialogueManager added.");
+		ReadTextFromFile();
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+	}
+
+	private void ReadTextFromFile() {
+		if (!File.Exists(textFilePath)) {
+			throw new IOException("Dialogue file cannot be found.");
+		}
+		GD.Print("Dialogue file found.");
+
+		textDict.Clear(); 
+
+		using (StreamReader reader = new StreamReader(textFilePath)) {
+			string line = reader.ReadLine();
+			string[] columnHeaders = line.Split("\t");
+			int translationIndex = 0;
+
+			for (int i = 1; i < columnHeaders.Length; i++) {
+				if (columnHeaders[i].Trim().Equals(language)) {
+					translationIndex = i;
+					break;
+				}
+			}
+
+			if (translationIndex == 0) {
+				throw new IOException("Text for selected language not found.");
+			}
+
+			while ((line = reader.ReadLine()) != null) {
+				GD.Print(line);
+				string[] cells  = line.Split("\t");
+				textDict.Add(cells[0].Trim(), cells[translationIndex].Trim());
+			}
+		}
 	}
 }
