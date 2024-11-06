82956a1028662fc440ce677b96dd82941a8fad53
Lawrence Yim <lawrence.yim.010@gmail.com>
2024-11-06 18:25:26 -0500

    Added in progress work for a startup menu to let player choose screen size.

---
+++ src/scripts/ui/WindowSize.cs
@@ -0,0 +1,24 @@
+/* 
+	Add as Autoload
+
+*/
+
+using Godot;
+
+public partial class WindowSize : Node {
+    Vector2I[] _sizes = new Vector2I[] {
+        new Vector2I(2560, 1440),
+        new Vector2I(1920, 1080),
+        new Vector2I(1280, 720),
+        new Vector2I(640, 360)
+    };
+
+    public override void _Ready() {
+        SetScreenSize(_sizes[3]);
+
+    }
+
+    public void SetScreenSize(Vector2I size) {
+        GetWindow().Size = size;
+    }
+}
