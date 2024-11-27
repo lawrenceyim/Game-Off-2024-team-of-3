71d57e071995bf86044b7dc9af3752eaee98ea47
Lawrence Yim <lawrence.yim.010@gmail.com>
2024-11-27 02:36:49 -0500

    Removed commented code used for converting player camera's gdscript code to C#.

--- src/game_object/player/game_camera/PlayerCamera.cs
+++ src/game_object/player/game_camera/PlayerCamera.cs
@@ -56,35 +56,3 @@ public partial class PlayerCamera : Camera2D {
 		Offset = newOffset;
 	}
 }
-
-/*
-
-
-#adding trauma is what sets off the shake effect, 
-#a timer makes sure a 2nd shake cant happen until the first one is done,
-#the current parameters have a nice balance for the shake.
-#timer is set to 0.2s oneshot = true
-
-func _physics_process(delta: float) -> void:
-	if !Global.player.weapon_sprite.inventory_mode:
-		var target_position = target.aim_position * sensitivity
-		position = position.lerp(target_position, 0.25)
-
-	if trauma:
-		trauma = max(trauma - decay * delta, 0)
-		_noise_y += NOISE_SPEED
-		_shake()
-		
-	if timer.is_stopped():
-		trauma = 0
-	
-func add_trauma(amount: float):
-	trauma = min(trauma + amount, 1.0)
-	timer.start()
-	
-func _shake():
-	var amount = pow(trauma, trauma_power)
-	offset.x = amplitude * amount * noise.get_noise_2d(noise.seed, _noise_y)
-	offset.y = amplitude * amount * noise.get_noise_2d(noise.seed * 2, _noise_y)
-
-*/
