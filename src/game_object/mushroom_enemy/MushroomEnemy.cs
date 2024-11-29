e2de157a5e45f9b57bdad81fc7ccb872746f2f9e
Lawrence Yim <lawrence.yim.010@gmail.com>
2024-11-28 22:52:27 -0500

    Added AI for mushroom enemy.

--- src/game_object/mushroom_enemy/MushroomEnemy.cs
+++ src/game_object/mushroom_enemy/MushroomEnemy.cs
@@ -1,12 +1,125 @@
 using Godot;
 using System;
 
-public partial class MushroomEnemy : Node {
-	// Called when the node enters the scene tree for the first time.
+public partial class MushroomEnemy : CharacterBody2D, IDamageable {
+	private const float WanderingSpeed = 100f;
+	private const double MinWanderTime = 3f;
+	private const double MaxWanderTime = 5f;
+	private const int BaseHealth = 2;
+	private const double AttackCooldown = .5f;
+	private const int AttackDamage = 1;
+	private const float DetectionRange = 500;
+	private const float CloseEnoughRange = 10f;
+	private const string WanderingState = "wander";
+	private const string PursuitState = "pursue";
+	private const string DeathState = "death";
+	private const string MoveAnimation = "move";
+	[Export] private AnimatedSprite2D _sprite;
+	[Export] private AlertLabel _alertLabel;
+	[Export] private HitFlash _hitFlash;
+	private StateMachine _stateMachine;
+	private PlayerCharacter _player;
+	private MeleeAttack _meleeAttack;
+	private Wander _wander;
+	private Health _health;
+	private bool _touchingPlayer = false;
+
 	public override void _Ready() {
+		PlayerCharacter.GetInstanceWithCallback((PlayerCharacter player) => {
+			_player = player;
+
+			_wander = new Wander(this, TimerUtil.CreateTimer(this, true), MinWanderTime, MaxWanderTime, WanderingSpeed);
+
+			SetStateMachine();
+		});
+
+		_health = new Health(BaseHealth);
+		_health.ZeroHealthEvent += (_, _) => _stateMachine.SwitchState(DeathState);
+
+		_meleeAttack = new MeleeAttack(TimerUtil.CreateTimer(this, true), AttackCooldown, AttackDamage);
+
+		_sprite.Play(MoveAnimation);
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta) {
+	public void TakeDamage(int damage) {
+		_hitFlash.DisplayHitFlash();
+		_health.DecreaseHealth(damage);
+	}
+
+	private void SetStateMachine() {
+		AiState wanderState = new AiState.Builder(WanderingState)
+			.SetStart(() => {
+				_wander.SetWanderingVelocity();
+			})
+			.SetExit(() => {
+				_wander.StopWandering();
+			})
+			.SetUpdate((double delta) => {
+				MoveAndSlide();
+			})
+			.SetPhysicsUpdate((double delta) => {
+				if (Position.DistanceTo(_player.Position) <= DetectionRange) {
+					_stateMachine.SwitchState(PursuitState);
+					return;
+				}
+			})
+			.Build();
+
+		AiState pursueState = new AiState.Builder(PursuitState)
+			.SetStart(() => {
+				_alertLabel.DisplayExclamationMark();
+			})
+			.SetExit(() => { })
+			.SetUpdate((double delta) => { })
+			.SetPhysicsUpdate((double delta) => {
+				float distanceFromTarget = Position.DistanceTo(_player.Position);
+				if (distanceFromTarget > DetectionRange) {
+					_alertLabel.DisplayQuestionMark();
+					_stateMachine.SwitchState(WanderingState);
+					return;
+				}
+				if (distanceFromTarget > CloseEnoughRange) {
+					Velocity = (_player.Position - Position).Normalized() * WanderingSpeed;
+					MoveAndSlide();
+				}
+
+				if (_touchingPlayer) {
+					_meleeAttack.AttackIfReady(_player);
+				}
+			})
+			.Build();
+
+		AiState deathState = new AiState.Builder(DeathState)
+			.SetStart(() => {
+				// Play death SFX
+				// Play death animation
+				_stateMachine.GetCurrentState().onExit();
+			})
+			.SetExit(() => {
+				QueueFree();
+			})
+			.SetUpdate((double delta) => { })
+			.SetPhysicsUpdate((double delta) => { })
+			.Build();
+
+		_stateMachine = new StateMachine.Builder(WanderingState)
+			.AddState(wanderState)
+			.AddState(pursueState)
+			.AddState(deathState)
+			.Build();
+
+		AddChild(_stateMachine);
+	}
+
+	private void _on_hitbox_area_2d_area_entered(Area2D body) {
+		if (body.GetParent() is PlayerCharacter) {
+			_touchingPlayer = true;
+		}
+	}
+
+	private void _on_hitbox_area_2d_area_exited(Area2D body) {
+		if (body.GetParent() is PlayerCharacter) {
+			_touchingPlayer = false;
+		}
 	}
 }
